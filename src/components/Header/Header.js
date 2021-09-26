62745d1d268c6a10e027ebcf4ea2389b5784393d
Hasibul294 <hasibulhasan294@gmail.com>
2021-09-26 17:59:38 +0600

    Readme file modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h2>Making a Drama Series: "Hablu Jokhon Programmer!"</h2>
-      <small> Making your Team as you Wish!!</small>
+      <h2>Drama Series: "Hablu Jokhon Programmer!"</h2>
+      <small>
+        {" "}
+        Making your Drama Team. You can choose your favorite parson.!!
+      </small>
       <h2>Total Budget: 5000000$</h2>
     </div>
   );
