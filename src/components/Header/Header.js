95562b7605f7435d7be217cf5c9139d5e683d47c
Hasibul294 <hasibulhasan294@gmail.com>
2021-09-26 20:46:57 +0600

    logo and website name added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,19 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h2>Drama Series: "Hablu Jokhon Programmer!"</h2>
-      <small>
-        {" "}
-        Making your Drama Team. You can choose your favorite parson.!!
-      </small>
-      <h2>Total Budget: 5000000$</h2>
+    <div>
+      <div className="header-title">
+        <img src="/image/theater.png" alt="" />
+        <h2 className="web-title">Drama Buzz</h2>
+      </div>
+      <div className="header-body">
+        <h2>Drama Series: "Hablu Jokhon Programmer!"</h2>
+        <small>
+          {" "}
+          Making your Drama Team. You can choose your favorite parson.!!
+        </small>
+        <h2>Total Budget: 5000000$</h2>
+      </div>
     </div>
   );
 };
